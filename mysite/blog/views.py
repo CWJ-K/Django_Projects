d42543c4dc4e6a2f1fa5715894173b1fdae0a51b
CWJ-K <wanju412@gmail.com>
2023-05-03 19:34:08 +0800

    feat: class-based view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-# Create your views here.
+# Create your views here. function-based view
 def post_list(request):
     posts = Post.published.all()
     # Pagination with 3 posts per page
@@ -29,3 +30,11 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+# use a class-based view
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
